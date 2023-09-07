887c1bc247691e143ad44434437ce3ed49b7bea4
soumya <soumyashankar.ghosh@gmail.com>
2023-09-07 21:01:58 +0530

    first commit

---
+++ server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello-world", handleHellowWorld)
+	http.HandleFunc("/health", handleHealth)
+
+	addr := "localhost:8000"
+	log.Printf("Listening on %s ...", addr)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleHellowWorld(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(
+			writer,
+			http.StatusText(http.StatusMethodNotAllowed),
+			http.StatusMethodNotAllowed,
+		)
+	}
+
+	writeResponse(writer, "Hello World")
+}
+
+func handleHealth(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(
+			writer,
+			http.StatusText(http.StatusMethodNotAllowed),
+			http.StatusMethodNotAllowed,
+		)
+	}
+
+	writeResponse(writer, "OK!")
+}
+
+func writeResponse(writer http.ResponseWriter, responseString string) {
+	response := []byte(responseString)
+	fmt.Println(response)
+	_, err := writer.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
